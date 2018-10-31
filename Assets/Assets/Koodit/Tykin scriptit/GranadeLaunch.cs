35ff0e4cea61a66a4eb9ba8101eb6ff48503c395
DESKTOP-R83GNJ3\Naapurin kone <nissi.petri@hotmail.com>
2018-10-31 11:16:46 +0200

    Tykin lataus yms. paskaa

--- Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
+++ Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
@@ -10,7 +10,7 @@ public class GranadeLaunch : MonoBehaviour
     public Rigidbody2D myRB;
     private float attackTimer = 0;
     private float attackCd = 1;
-
+    private float force = 4;
 
     void Start()
     {
@@ -20,17 +20,22 @@ public class GranadeLaunch : MonoBehaviour
     void Update()
     {
         HandleAttacks();
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log(force);
+            force += Time.deltaTime * 25;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             if (attackTimer <= 0)
             {
                 Rigidbody2D Granade;
                 Granade = Instantiate(myRB, Cannon.position, Cannon.rotation) as Rigidbody2D;
-                Granade.AddForce(Cannon.right * 25);
+                Granade.AddForce(Cannon.right * force);
 
                 attackTimer = attackCd;
-                DestroyObject(myRB, 1f);
+                force = 0;
+                Destroy(Granade, 5f);
             }
         }
     }
