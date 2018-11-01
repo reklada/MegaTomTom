fc89f54acf8de7f76d60167c9cd03cc9f76734ff
DESKTOP-UNC7O72\Nissi <petri.nissi@edu.lapinamk.fi>
2018-11-01 08:41:04 +0200

    uus granade launch

--- Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
+++ Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
@@ -10,13 +10,9 @@ public class GranadeLaunch : MonoBehaviour
     public Rigidbody2D myRB;
     private float attackTimer = 0;
     private float attackCd = 1;
-<<<<<<< HEAD:Assets/Assets/Koodit/GranadeLaunch.cs
 
-    private float force;
     private float forceIncrease = 2;
-=======
     private float force = 4;
->>>>>>> 35ff0e4cea61a66a4eb9ba8101eb6ff48503c395:Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
 
     void Start()
     {
@@ -26,16 +22,13 @@ public class GranadeLaunch : MonoBehaviour
     void Update()
     {
         HandleAttacks();
-<<<<<<< HEAD:Assets/Assets/Koodit/GranadeLaunch.cs
         if (Input.GetKeyDown(KeyCode.Space))
-=======
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log(force);
             force += Time.deltaTime * 25;
         }
         if (Input.GetKeyUp(KeyCode.Space))
->>>>>>> 35ff0e4cea61a66a4eb9ba8101eb6ff48503c395:Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
         {
             Debug.Log(force);
             force += forceIncrease*Time.deltaTime;
@@ -49,13 +42,9 @@ public class GranadeLaunch : MonoBehaviour
                 Granade.AddForce(Cannon.right * force);
 
                 attackTimer = attackCd;
-<<<<<<< HEAD:Assets/Assets/Koodit/GranadeLaunch.cs
                 forceIncrease = 0;
-                DestroyObject(myRB, 1f);
-=======
                 force = 0;
                 Destroy(Granade, 5f);
->>>>>>> 35ff0e4cea61a66a4eb9ba8101eb6ff48503c395:Assets/Assets/Koodit/Tykin scriptit/GranadeLaunch.cs
             }
         }
     }
