9a1fb93f5073df7b58aa99d02741e95fff622bf6
reklada <reklada@gmail.com>
2018-11-14 19:47:13 +0200

    Sisältää menun, monsteri spawni koodin (valinnainen käyttääkö lopulta), ja jotain uudelleen nimetty

---
+++ Assets/Assets/Koodit/Monsterit/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject Auto; 
+    public GameObject enemy;
+    float randX;
+    float xxx;
+    float z;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+            if (Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnRate;
+                randX = Random.Range(10f, 40f);
+                xxx = Auto.transform.position.x + randX;
+                whereToSpawn = new Vector2(xxx, 0.5f);
+                Instantiate(enemy, whereToSpawn, Quaternion.identity);
+            }
+	}
+}
--- Assets/Assets/Menu/MainMenu.cs
+++ Assets/Assets/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame ()
     {
-        SceneManager.LoadScene("Demo");
+        SceneManager.LoadScene("Ajaminen");
     }
 
     public void QuitGame ()
